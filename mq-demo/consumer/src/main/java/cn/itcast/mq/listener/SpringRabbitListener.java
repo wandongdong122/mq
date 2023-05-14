255225787f87ce3877bfc3fcf9a88c934378b9e4
wdd <wandongdong122@163.com>
2023-05-14 20:15:01 +0800

    fanoutQueue

--- mq-demo/consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitListener.java
+++ mq-demo/consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitListener.java
@@ -24,21 +24,4 @@ public class SpringRabbitListener {
         System.err.println("消费者2接收到fanout.queue2的消息：【"+ msg +"】");
     }
 
-    @RabbitListener(bindings = @QueueBinding(
-            value = @Queue(name = "direct.queue1"),
-            exchange = @Exchange(name = "itcast.direct",type = ExchangeTypes.DIRECT),
-            key = {"red","blue"}
-    ))
-    public void listenDirectQueue1(String msg){
-        System.out.println("消费者接收到direct.queue2的消息：【"+ msg +"】");
-    }
-
-    @RabbitListener(bindings = @QueueBinding(
-            value = @Queue(name = "direct.queue2"),
-            exchange = @Exchange(name = "itcast.direct",type = ExchangeTypes.DIRECT),
-            key = {"red","yellow"}
-    ))
-    public void listenDirectQueue2(String msg){
-        System.out.println("消费者接收到direct.queue2的消息：【"+ msg +"】");
-    }
 }
